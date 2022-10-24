27f7cd4daa31e4e96135e79d88a5c42fc4e03f57
turuslan <turuslan.devbox@gmail.com>
2022-10-24 15:32:42 +0300

    remove packed
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- Include/WAVM/Platform/Error.h
+++ Include/WAVM/Platform/Error.h
@@ -4,11 +4,11 @@
 #include "WAVM/Inline/BasicTypes.h"
 
 namespace WAVM { namespace Platform {
-	WAVM_PACKED_STRUCT(struct AssertMetadata {
+	struct AssertMetadata {
 		const char* condition;
 		const char* file;
 		U32 line;
-	});
+	};
 
 	WAVM_API void handleAssertionFailure(const AssertMetadata& metadata);
 	[[noreturn]] WAVM_API void handleFatalError(const char* messageFormat,
